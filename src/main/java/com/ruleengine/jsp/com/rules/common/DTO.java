0e9f65b076738c34a0df8a08cadbf1519a60b1ac
wbadmin <>
2023-04-06 14:31:50 +0200

    {/src/main/java/com/ruleengine/jsp/com/rules/common/DTO.java}

--- src/main/java/com/ruleengine/jsp/com/rules/common/DTO.java
+++ src/main/java/com/ruleengine/jsp/com/rules/common/DTO.java
@@ -6,12 +6,33 @@ package com.ruleengine.jsp.com.rules.common;
 
 public class DTO implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public DTO() {
-    }
+	private java.lang.Integer id;
+	private java.lang.String name;
 
+	public DTO() {
+	}
 
+	public java.lang.Integer getId() {
+		return this.id;
+	}
 
+	public void setId(java.lang.Integer id) {
+		this.id = id;
+	}
+
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public DTO(java.lang.Integer id, java.lang.String name) {
+		this.id = id;
+		this.name = name;
+	}
 
 }
